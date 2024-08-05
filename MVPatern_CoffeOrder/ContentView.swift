6f13065ca6b51f6557701ad60c6b95e2d4f94c5e
qlap04 <uchihaquoc04@gmail.com>
2024-08-06 01:54:34 +0700

    Initial Commit

---
+++ MVPatern_CoffeOrder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVPatern_CoffeOrder
+//
+//  Created by Phan Quốc Lập on 06/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
